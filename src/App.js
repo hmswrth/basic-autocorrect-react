fa2421ea1ec01ac5269051f3c179b1813cae2ce3
Hemanth Mudra <mudra.hemanth@gmail.com>
2022-01-15 02:22:50 +0530

    prod code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AutocorrectTextarea from './components/AutocorrectTextarea';
+
+function App() {
+  return (
+    <div className="App">
+    <AutocorrectTextarea />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AutocorrectTextarea.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+
+function AutocorrectTextarea() {
+   const corrections = {
+      helo: "hello",
+      wrld: "world"
+   };
+
+   const [data, setData] = useState("");
+
+   useEffect(() => {
+      if (data[data.length - 1] === " ") {
+         let words = data.split(" ");
+         words.pop();
+         let word = words[words.length - 1];
+         if (word in corrections) {
+            let correctedData = data.replace(word, corrections[word]);
+            setData(correctedData);
+         }
+         else {
+            console.log('no replacement found')
+         }
+      }
+   }, [data]);
+
+   const handleData = (e) => {
+      setData(e.target.value);
+   }
+
+   return (
+      <>
+         <label for='textarea'>Start typing below</label> <br />
+         <textarea
+            data-testid="textarea"
+            rows={5}
+            cols={50}
+            onChange={handleData}
+            value={data}></textarea>
+      </>
+   )
+}
+
+export default AutocorrectTextarea
